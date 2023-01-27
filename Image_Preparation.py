42a270bd75e49fa1f496cd58e427e2c1eff6594a
selinasuzeroglu <selina.suzeroglu@gmail.com>
2023-01-27 16:18:31 +1100

    Commit

--- Image_Preparation.py
+++ Image_Preparation.py
@@ -19,7 +19,6 @@ import numpy as np
 from PIL import Image
 
 
-
 # 0) Prepare Images: Load and normalize the train and test data
 
 class ImageDataset(Dataset):
@@ -67,11 +66,11 @@ class ImageDataset(Dataset):
 
 img_dir = 'C:\\Users\\ssuz0008\\PycharmProjects\\UVVis_3.0\\Main_Arduino'
 csv = 'C:\\Users\\ssuz0008\\PycharmProjects\\PyTorch\\file.csv'
-data = ImageDataset(csv, img_dir)
+dataset = ImageDataset(csv, img_dir)  # dataset
 
 
 def show_image(idx):
-    image = data.__getitem__(idx)
+    image = dataset.__getitem__(idx)
     csv_file = pd.read_csv(csv)
     label = str(csv_file.iloc[idx, 1])
     print(f"Label: {label}")
@@ -79,4 +78,39 @@ def show_image(idx):
     plt.show()
 
 
+batch_size = 1
+num_workers = 1
 
+# train_dataset = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [3, 1])
+
+
+class Net(nn.Module):
+    ''' Models a simple Convolutional Neural Network'''
+
+    def __init__(self):
+        ''' initialize the network '''
+        super(Net, self).__init__()
+        # 3 input image channel, 6 output channels,
+        # 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        # Max pooling over a (2, 2) window
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5x5 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        ''' the forward propagation algorithm '''
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+print(net)
