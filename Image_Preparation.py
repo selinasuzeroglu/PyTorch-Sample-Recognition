4207dce5bec97ee1b71da735d27fd9009681ef33
selinasuzeroglu <selina.suzeroglu@gmail.com>
2023-01-30 17:31:18 +1100

    Commit

--- Image_Preparation.py
+++ Image_Preparation.py
@@ -71,32 +71,25 @@ class ImageDataset(Dataset):
 
         return image_tensor, img_class
 
-        """ ImageDataset.__getitem__(idx=1) returns tuple([0], [1]) with [0] = torch.tensor for image, 
-        [1] = string for corresponding label """
+
+""" ImageDataset.__getitem__(idx=str) returns tuple([0], [1]) 
+with [0] = torch.tensor for image, [1] = string for corresponding label """
 
 
 img_dir = 'C:\\Users\\ssuz0008\\PycharmProjects\\UVVis_3.0'
 csv = 'C:\\Users\\ssuz0008\\PycharmProjects\\PyTorch\\file.csv'
 
 dataset = ImageDataset(csv, img_dir)  # dataset
-"""For now, we are using one dataset for training and testing
- Later we use one other dataset wit different directory consisting of testing images """
 
 
 train_dataloader = torch.utils.data.DataLoader(dataset, shuffle=True)
-"""torch.Size([1, 3, 224, 224]) for batch_size = 1, chanel_input = 3 (RGB), pixel_height = 224, pixel_width = 224"""
+"""train_dataloader.Size([1, 3, 224, 224]) 
+for batch_size = 1, in_channels = 3 (RGB), pixel_height = 224, pixel_width = 224"""
 # test_dataloader = torch.utils.data.DataLoader(dataset, shuffle=False)
 # train_dataset, test_dataset = torch.utils.data.random_split(dataset, [2, 1])
 
 dataiter = iter(train_dataloader)  # creating iterator which represents dataset
-images, labels = next(dataiter)   # returning successive items in the iterator, i.e. tuple from dataset
-
-def imshow(img):
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
+images, labels = next(dataiter)   # returning successive items in the iterator, i.e. tuple([image], [label])
 
 
 # 2) Define Neural Network
@@ -106,26 +99,28 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size = width x height)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+        self.adapt = nn.AdaptiveAvgPool2d(5)
 
     def forward(self, x):
-        x = self.conv1(x)
-        #x = self.pool(F.relu(self.conv1(x)))
-        #x = self.pool(F.relu(self.conv2(x)))
-        #x = torch.flatten(x, 1) # flatten all dimensions except batch
-        #x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
-        return x.size()
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.adapt(x)
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 net = Net()
 
-print(next(dataiter)[0])
+print(net(images))
 
 
 # criterion = nn.CrossEntropyLoss()
