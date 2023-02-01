f39800d4d3092c7c0e7a38add602ea79038d0348
selinasuzeroglu <selina.suzeroglu@gmail.com>
2023-02-01 15:35:43 +1100

    Commit

--- Image_Preparation.py
+++ Image_Preparation.py
@@ -18,36 +18,55 @@ import matplotlib
 
 import numpy as np
 from PIL import Image
-from Custom_Dataset import ImageDataset
+from Custom_Dataset import ImageDataset, transform
+
+# Set Device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 """ ImageDataset.__getitem__(idx=str) returns tuple([0], [1]) 
 with [0] = torch.tensor for image, [1] = string for corresponding label """
 
-# direction of training images:
-train_img_dir = 'C:\\Users\\ssuz0008\\PycharmProjects\\UVVis_3.0\\Main_Arduino\\Photos'
-# direction of training csv.file:
-train_csv = 'C:\\Users\\ssuz0008\\PycharmProjects\\PyTorch\\file.csv'
+img_dir = 'C:\\Users\\ssuz0008\\PycharmProjects\\UVVis_3.0\\Main_Arduino\\Photos'
+csv = 'C:\\Users\\ssuz0008\\PycharmProjects\\PyTorch\\file.csv'
+
+batch_size = 1
 
 
-train_dataset = ImageDataset(train_csv, train_img_dir)
-train_dataloader = torch.utils.data.DataLoader(train_dataset, shuffle=True)
+dataset = ImageDataset(csv, img_dir, transform=transform)
+
+train_size = int(len(dataset)*.7)
+test_size = int(len(dataset)-train_size)
+
+train_set, test_set = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+train_dataloader = torch.utils.data.DataLoader(train_set, shuffle=True, batch_size=batch_size)
+test_dataloader = torch.utils.data.DataLoader(test_set, shuffle=False, batch_size=batch_size)
+
 """train_dataloader has elements consisting of tuple(image_tensor, labels) with image_tensor.Size([1, 3, 224, 224]) 
 for batch_size = 1, in_channels = 3 (RGB), pixel_height = 224, pixel_width = 224
 and labels.Size([1]) for integers representing class label"""
 
+
+classes = ('gelb', 'grau', 'orange')
+
+
+# function for showing images:
+def imshow(img):
+    img = img
+    npimg = img.numpy()  # convert to numpy objects
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# show training images:
+
 dataiter = iter(train_dataloader)  # creating iterator which represents dataset
 images, labels = next(dataiter)  # returning successive items in the iterator, i.e. tuple([image], [label])
-
-test_dataloader = torch.utils.data.DataLoader(train_dataset, shuffle=False)
-
-# showing images:
-def imshow(img):
-  ''' function to show image '''
-  img = img
-  npimg = img.numpy() # convert to numpy objects
-  plt.imshow(np.transpose(npimg, (1, 2, 0)))
-  plt.show()
+"""for batch_size = 4: images.Size([4, 3, 224, 224]) and labels.Size([4]), i.e. 4 images with associated 4 labels 
+next()_function gives a batch of additional 4 images with associated 4 labels, etc. etc. """
+# imshow(torchvision.utils.make_grid(images))
+# print(' '.join('%s' % classes[labels[j]] for j in range(train_size)))
 
 
 # 2) Define Neural Network
@@ -61,7 +80,7 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 3)
         self.adapt = nn.AdaptiveAvgPool2d(5)
 
     def forward(self, x):
@@ -75,7 +94,9 @@ class Net(nn.Module):
         return x
 
 
-net = Net()
+net = Net()  # torch.Size([batch_size, 10])
+
+
 
 # 3) Define Loss Function
 
@@ -99,21 +120,21 @@ for epoch in range(2):  # loop over the dataset multiple times
         # print statistics
         running_loss += loss.item()
         # print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-        # running_loss = 0.0
+        running_loss = 0.0
 
-PATH = 'C:\\Users\\ssuz0008\\PycharmProjects\\PyTorch\\image_net.pth'
-torch.save(net.state_dict(), PATH)
-
-classes = (0, 1, 2)
 # 5) Test Network
 
 dataiter = iter(test_dataloader)
 images, labels = next(dataiter)
 
-
+# show testing images
+imshow(torchvision.utils.make_grid(images))
+# print verified labels
+print('GroundTruth: ', ' '.join('%s' % classes[labels[j]] for j in range(batch_size)))
 
 outputs = net(images)
 
 _, predicted = torch.max(outputs, 1)
-print(' '.join('%s' % classes[predicted[j]] for j in range(1)))
-imshow(torchvision.utils.make_grid(images))
+# print predicted labels
+print('Predicted: ', ' '.join('%s' % classes[predicted[j]] for j in range(batch_size)))
+
